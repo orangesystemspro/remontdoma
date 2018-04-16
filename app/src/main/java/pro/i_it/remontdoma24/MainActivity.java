19083c64c846decea886860bd80ca72c95828aae
Andrii Drihulias <varfalomiy@gmail.com>
2018-04-16 16:19:21 +0300

    init project

---
+++ app/src/main/java/pro/i_it/remontdoma24/MainActivity.java
@@ -0,0 +1,32 @@
+package pro.i_it.remontdoma24;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final View logo = findViewById(R.id.logo);
+        final WebView web = findViewById(R.id.web);
+        web.setVisibility(GONE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                logo.setVisibility(GONE);
+                web.setVisibility(VISIBLE);
+                web.getSettings().setJavaScriptEnabled(true);
+                web.loadUrl("https://remontdoma24.ru/");
+            }
+        }, DELAY);
+    }
+}
